801b33b4a18bb6c6c0e35bd94d3839e4e3bef339
Vanessa Machuca <vdm02014@mymail.pomona.edu>
2018-07-10 09:22:37 -0700

    uncommented code in initializeSystem_2D; multiple cells with isStem,Pcap, isa... as vectors

--- subroutines_2D/initializeSystem_2D.m
+++ subroutines_2D/initializeSystem_2D.m
@@ -4,30 +4,32 @@ function [L, TUcells, IMcells, TUprop, IMprop] = initializeSystem_2D(N,M,TUpmax)
     L = false(N,M);
     % END INITIALIZE GRID  ----------------------------------------------
     
-    % START INITIALIZE TUMOR CELLS -------------------------------------------
-    TUcells = int32(N*round(M/2)-round(N/2)); % first TU cell is centered
-    
+    % START INITIALIZE TUMOR CELLS ------------------------------------------- 
     %initiate multiple tumor cells
-    %posa =int32(N*round(M/2)-round(N/2));
-    %posa =int32(N*round(M/2)-round(N/2)+1);
-    %posa =int32(N*round(M/2)-round(N/2)-1);
-    %posa =int32(N*round(M/2)-round(N/2)+N);
-    %posa =int32(N*round(M/2)-round(N/2)-N);
-    %posa =int32(N*round(M/2)-round(N/2)+N+1);
-    %TUcells = [posa posb posc posab posbc posac]; 
+    posa =int32(N*round(M/2)-round(N/2));
+    posb =int32(N*round(M/2)-round(N/2)+1);
+    posc =int32(N*round(M/2)-round(N/2)-1);
+    posab =int32(N*round(M/2)-round(N/2)+N);
+    posbc =int32(N*round(M/2)-round(N/2)-N);
+    posac =int32(N*round(M/2)-round(N/2)+N+1);
+    TUcells = [posa posb posc posab posbc posac]; 
     
     %set property of first cells: receptor expression type 
-    %TUprop.isa = true; 
-    %TUprop.isb = true; 
-    %TUprop.isc = true; 
-    %TUprop.isab = true; 
-    %TUprop.isbc = true; 
-    %TUprop.isac = true; 
+    TUprop.isa = true; 
+    TUprop.isb = true; 
+    TUprop.isc = true; 
+    TUprop.isab = true; 
+    TUprop.isbc = true; 
+    TUprop.isac = true; 
     
+    TUprop.Pcap = [uint8(TUpmax) uint8(TUpmax) uint8(TUpmax) uint8(TUpmax) uint8(TUpmax) uint8(TUpmax)]; % set property of first cell: proliferation capacity
+    TUprop.isStem = [true true true true true true];        % set property of first cell: stemness
     
-    L(TUcells) = true; 			 % place first tumor cell on the grid
-    TUprop.isStem = true;        % set property of first cell: stemness
-    TUprop.Pcap = uint8(TUpmax); % set property of first cell: proliferation capacity
+    %original code: 
+    %TUcells = int32(N*round(M/2)-round(N/2)); % first TU cell is centered
+    %OG L(TUcells) = true; 			 % place first tumor cell on the grid
+    %OG TUprop.isStem = true;        % set property of first cell: stemness
+    %OG Uprop.Pcap = uint8(TUpmax); % set property of first cell: proliferation capacity
     
     % END INITIALIZE TUMOR CELLS ---------------------------------------------
 
