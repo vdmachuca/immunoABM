1342cae57386da24b1be86a6f024922c669b22d9
Vanessa Machuca <vdm02014@mymail.pomona.edu>
2018-07-18 14:48:42 -0700

    IMinflux and getAdjacent_2D are still disagreeing. Will be changing IMinflux and debugging getAdjacent_2D to get to the bottom of the issue.

--- subroutines_ND/IMinflux.m
+++ subroutines_ND/IMinflux.m
@@ -1,18 +1,38 @@
 function [L,IMcells,IMprop] = IMinflux(L,IMcells,IMprop,IMpmax,IMkmax,IMinflRate)
 
+% if IMinflRate>0 % if an immune influx is desired
+%     
+% % if there are empty locations
+% if sum(~L(:))>0
+% % place N immune cells in empty locations
+% [~,coordsNewIMcells] = datasample(L(:),IMinflRate,'Replace',false,'Weights',uint8(~L(:)));
+% 
+% 
+% % InfluxMap = rand(size(L)) <= IMinfluxProb; % find random places
+% % InfluxMap(L) = false; % only empty grid cells
+% 
+% L(coordsNewIMcells) = true; % place new cells on grid
+% nNewCells = numel(coordsNewIMcells); % number of new immune cells
+% IMcells = [IMcells, coordsNewIMcells]; % add new cells to stack
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%have IMinflRate CARs flow in from the point source
 if IMinflRate>0 % if an immune influx is desired
     
-% if there are empty locations
 if sum(~L(:))>0
-% place N immune cells in empty locations
-[~,coordsNewIMcells] = datasample(L(:),IMinflRate,'Replace',false,'Weights',uint8(~L(:)));
+    
+%place N immune cells in empty locations
+[~,coordsNewIMcells] = datasample(L(500:100000),IMinflRate,'Replace',false,'Weights',uint8(~L(500:100000)));
+%[~,coordsNewIMcells] = datasample(L(:),IMinflRate,'Replace',false);
 
-% InfluxMap = rand(size(L)) <= IMinfluxProb; % find random places
-% InfluxMap(L) = false; % only empty grid cells
+%coordsNewIMcells = length(L); 
 
 L(coordsNewIMcells) = true; % place new cells on grid
+
 nNewCells = numel(coordsNewIMcells); % number of new immune cells
 IMcells = [IMcells, coordsNewIMcells]; % add new cells to stack
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 IMprop.Pcap = [IMprop.Pcap, repmat(IMpmax,1,nNewCells)];  % add properties
 IMprop.Kcap = [IMprop.Kcap, repmat(IMkmax,1,nNewCells)];  % add properties
--- subroutines_ND/getSystemParams.m
+++ subroutines_ND/getSystemParams.m
@@ -24,8 +24,11 @@ mySystem.params.IMpkill = 0.1;      % probability of killing, default 0.1
 mySystem.params.IMpdeath = 1-(1-0.0037)^4;  % HISTO GENERATED - probability of spontaneous death
 mySystem.params.IMrwalk = 0.8;      % random influence on movement, default 0.75
 mySystem.params.IMspeed = 97;       % speed of immune cell movement, default 97
+
 mySystem.params.IMinfluxProb = 0.3; % probability of immune cell influx, def. 0.72
+
 mySystem.params.IMinflRate = 1;     % how many immune cells appear simultaneously
+
 mySystem.params.engagementDuration = 48; % how many intermed. steps is a killing cell engaged? default 48 (=6 hrs)
 % END INITIALIZE LYMPHOCYTES --------------------------------------------
 
